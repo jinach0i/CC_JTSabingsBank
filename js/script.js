f020c6b88d8abea1c64269af3a07ba14f93a2f73
jinach0i <ginach0irk@gmail.com>
2022-04-21 17:03:05 +0900

    css added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-const slideList=document.querySelector('.main-banners ul li') 
-slideFrame=document.querySelector('.main-banners'),
-slideLen=document.querySelector('.')
+AOS.init();
+var sectionCar = document.querySelectorAll('.section-carousels'),
+    mainCar = document.querySelectorAll('.main-carousels');
+
+
+var sectionCar = new Swiper(".section-carousels", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: false,
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+  });
+
+var mainCar = new Swiper(".main-carousels", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    //  autoplay: {
+    //    delay: 3000,
+    //    disableOnInteraction: false,
+    //  },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+// sectionCar.controller.control = mainCar;
+// mainCar.controller.control = sectionCar;
