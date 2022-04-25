1b56aa6c123befa3dd9c170dc63d4a502aff6f5c
jinach0i <ginach0irk@gmail.com>
2022-04-25 18:01:12 +0900

    edited menu

--- js/jquery.js
+++ js/jquery.js
@@ -1,19 +1,7 @@
 $(function () {
-    var slide=$('#campaign ul li'),
-    current = 0;
-    setInterval(()=>{
-    var prev = slide.eq(current);
-    current++;
-    move(prev, 1, 0);
-    if(current==slide.size()){
-    current=0;
-    };
-    var next = slide.eq(current);
-    move(next, 0, 1);
-    },3000);
-    function move(tg, start, end){
-    tg.css({opacity: start}).stop().animate({opacity:end});
-    };
-
-    
+    $('.menu-btn').on('click',function () {
+        $('#gnb').css('display','block');
+        $('#header').css('background','white');
+        
+    });
 })
